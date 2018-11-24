b9167211c86d238b6e6921cdd464490871de6a55
Zeeshan Khan <zee.khan34@gmail.com>
2018-11-24 16:04:54 +0500

    no message

--- screenshot.sh
+++ screenshot.sh
@@ -13,27 +13,29 @@ echo "done"
 
 #------------------------------------------------------
 echo "installing Aquatone"
-wget https://github.com/michenriksen/aquatone/releases/download/v1.4.1/aquatone_linux_amd64_1.4.1.zip
-unzip aquatone_linux_amd64_1.4.1.zip -d ~/tools/aquatone
-cd ~/tools/aquatone
-chmod +x ./aquatone
-sudo ln -s ~/tools/aquatone/aquatone /usr/bin/aquatone
-echo "done"
-
-
-
-#------------------------------------------------------
-echo "installing EyeWitness"
-if [ ! -d ./EyeWitness* ]; then
-    git clone https://github.com/FortyNorthSecurity/EyeWitness.git
-    pyenv local 2.7.15
-    cd EyeWitness
-    cd setup
-    chmod +x ./setup.sh
-    sudo ./setup.sh
+if [ ! -d ./aquatone* ]; then
+    wget https://github.com/michenriksen/aquatone/releases/download/v1.4.2/aquatone_linux_amd64_1.4.2.zip
+    unzip aquatone_linux_amd64_1.4.2.zip -d ~/tools/aquatone
+    cd ~/tools/aquatone
+    chmod +x ./aquatone
+    #sudo ln -s ~/tools/aquatone/aquatone /usr/bin/aquatone
 fi
 cd ~/tools/
 echo "done"
+
+
+#------------------------------------------------------
+# echo "installing EyeWitness"
+# if [ ! -d ./EyeWitness* ]; then
+#     git clone https://github.com/FortyNorthSecurity/EyeWitness.git
+#     pyenv local 2.7.15
+#     cd EyeWitness
+#     cd setup
+#     chmod +x ./setup.sh
+#     sudo ./setup.sh
+# fi
+# cd ~/tools/
+# echo "done"
 
 
 
